78b5fafb06e085381115c93f826bec51e8d9a402
RuxinChen <32134331+RuxinChen@users.noreply.github.com>
2018-05-31 23:14:11 -0500

    run this file!

--- Data/compete.py
+++ Data/compete.py
@@ -6,15 +6,13 @@ from mr3px.csvprotocol import CsvProtocol
 import csv
 from math import radians, cos, sin, asin, sqrt
 ########################################################################
-# python3 competition.py --file restaurant.csv restaurant.csv
 # python3 compete.py --file neighbor.csv neighbor.csv > neighbor_d10.csv
 ########################################################################
 
-#df = pd.read_csv('sample.csv', sep=",")
 
 class MRPair(MRJob):
 
-    #OUTPUT_PROTOCOL = CsvProtocol
+    OUTPUT_PROTOCOL = CsvProtocol
 
     def mapper_init(self):
         self.df = df 
@@ -22,20 +20,12 @@ class MRPair(MRJob):
 
     def mapper(self, _, line):
         line = np.array(line.split(','))
-        #self.num += 1
-        #print(self.num)
-        #line = next(csv.reader([line]))
-        #print(line)
         id1, lat1, lng1, city1= line[0], line[1], line[2], line[3][2:][:-1]
         for i, row in self.df.iterrows():
             id2, lat2, lng2, city2 = row[0], row[1], row[2], row[3][2:][:-1]
-            #print(id2)
-            #print(lat2)
-            print(city1)
             if city1 in self.lst_of_city:
                 print(city1)
                 if id2 != id1 and id2 != None and city1 == city2:
-                    #yield (id1, id2), (lat1, lng1, lat2, lng2)
                     l = [id1, id2]
                     l.sort()
                     if l[0] == id1: 
@@ -47,13 +37,10 @@ class MRPair(MRJob):
         location = list(value)[0]
         lat1, lng1= float(location[0]), float(location[1])
         lat2, lng2= float(location[2]), float(location[3])
-        #print(lat2)
         h = haversine(lat1, lng1, lat2, lng2)
         #levenshtein = self.levenshtein(cat1, cat2)
         #overlap_cat = self.category(cat1, cat2)
         yield key, h
-        #except:
-        #    yield None, None
 
     def reducer(self, key, haversine):
         haversine = list(haversine) 
@@ -64,7 +51,7 @@ class MRPair(MRJob):
     def reducer_final(self, key, value):
         lst = list(value)
         yield key, lst
-        #yield (None, [key]+lst) 
+
 
     def steps(self):
         return [MRStep(mapper_init=self.mapper_init,
@@ -138,7 +125,6 @@ def haversine(lat1, lng1, lat2, lng2):
         MAX_PRICE = 4
         return MAX_PRICE - abs(range1 - range2)
 ##########################################################################
-
 
 if __name__ == '__main__':
 
